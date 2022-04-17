57709dac880a7d798d2f63d24c5c3f38979e3048
SIO TOU LAI [Nelson] <laisiotou1997@gmail.com>
2022-04-17 21:17:37 +1000

    Update knightstour.py

--- knightstour.py
+++ knightstour.py
@@ -1,14 +1,53 @@
 
+
+
 from board import Board
 import pygame
 from gui import GUI
+from algorithm import Algorithm
 
 WINDOWS_SIZE = [500, 500]
+DEBUG_MODE = False
+
 class KnightsTour:
-    def __init__(self, board_size:tuple, start_pos:tuple, algo_approach) -> None:
+    def __init__(self, board_size:tuple, start_pos:tuple, algo_approach=None) -> None:
         self._board_size = board_size
         self._board = Board(board_size, start_pos)
+        self._gui = None
+        self._algo = algo_approach
         self.start()
+
+    def _curser_debug(self, event):
+        # False = can continue
+        if(self._board.get_step_count() == pow(self._board.get_size(), 2)):
+            self._gui.set_caption('Congratulation, You Solved The Problem!')
+            print("Congratulation, You Solved The Problem!\nPath:")
+            self._board.show_path()
+            return False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_pos = pygame.mouse.get_pos()
+            x, y = self._board.get_mouse_tile_pos(mouse_pos, WINDOWS_SIZE)
+            target_pos = {'x': x, 'y': y}
+
+            try:
+                if (self._board.move(target_pos) is True):
+                    self._gui.draw_board()
+                    self._gui.draw_path(self._board.get_paths(), target_pos, self._board.get_size())
+
+                    # check current tile see if any legel move avaliable.
+                    self._board.check_legel_move(target_pos, target_pos)
+            except Board.NotSloveException:
+                print("Unfortunally, You Did Not Solved The Problem!\nPath:")
+                self._board.show_path()
+                return False
+        
+        return True
+    
+    def _slove(self, algo):
+        approach = Algorithm(self._board, algo)
+        return approach.get_result()
+
 
     def start(self):
         pygame.init()
@@ -20,34 +59,27 @@ class KnightsTour:
 
         # Run until the user asks to quit
         running = True
-        pause = False
         self._gui.draw_board()
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-                if(self._board.get_step_count() == self._board.get_size() * self._board.get_size()):
-                    pause = True
-                    self._gui.set_caption('Congratulation, You Solved The Problem!')
-                    print("Congratulation, You Solved The Problem!\nPath:")
-                    self._board.show_path()
-
-                if event.type == pygame.MOUSEBUTTONUP and pause is False:
-                    mouse_pos = pygame.mouse.get_pos()
-                    x, y = self._board.get_mouse_tile_pos(mouse_pos, WINDOWS_SIZE)
-                    target_pos = {'x': x, 'y': y}
-
-                    try:
-                        if (self._board.move(target_pos) is True):
+                if (DEBUG_MODE is True):
+                    running = self._curser_debug(event)
+                else:
+                    solution = self._slove(self._algo)
+                    if len(solution[0]) == pow(self._board.get_size(), 2):
+                        # play movement
+                        for move in solution[0]:
+                            target_pos = {'x': move[0], 'y': move[1]}
+                            self._board.move(target_pos)
                             self._gui.draw_board()
-                            self._gui.draw_path(self._board.get_path(), target_pos, self._board_size)
+                            self._gui.draw_path(self._board.get_paths(), target_pos, self._board.get_size())
+                            self._gui.refresh()
+                            pygame.time.wait(1000)
 
-                            # check current tile see if any legel move avaliable.
-                            self._board.check_legel_move(target_pos, target_pos)
-                    except Board.NotSloveException:
-                        print("Unfortunally, You Did Not Solved The Problem!\nPath:")
-                        self._board.show_path()
+                        running = False
 
                 self._gui.refresh()
 
