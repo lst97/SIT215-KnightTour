6976ec80c5c9fd5f2c821d8a235f8f8611984da3
SIO TOU LAI [Nelson] <laisiotou1997@gmail.com>
2022-04-26 18:56:08 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from knightstour import KnightsTour
 from algorithm import Algorithm
 
-BOARD_SIZE = (5, 5)
+BOARD_SIZE = (12, 12)
 START_POS = {'x': 0, 'y': 0}
 
 def main():
-    approach = Algorithm.BT # replace with different approach algorithm
+    approach = Algorithm.Warnsdorff # replace with different approach algorithm
     KnightsTour(BOARD_SIZE, START_POS, approach)
 
 main()
