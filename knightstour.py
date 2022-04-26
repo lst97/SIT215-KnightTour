34dbaf00ea6f7a632a6f39588c0b939bc0034580
SIO TOU LAI [Nelson] <laisiotou1997@gmail.com>
2022-04-26 18:54:58 +1000

    Update knightstour.py

--- knightstour.py
+++ knightstour.py
@@ -1,6 +1,4 @@
 
-
-
 from board import Board
 import pygame
 from gui import GUI
@@ -13,12 +11,12 @@ class KnightsTour:
     def __init__(self, board_size:tuple, start_pos:tuple, algo_approach=None) -> None:
         self._board_size = board_size
         self._board = Board(board_size, start_pos)
-        self._gui = None
+        self._gui = GUI(WINDOWS_SIZE, pygame, self._board.get_tiles())
         self._algo = algo_approach
         self.start()
 
     def _curser_debug(self, event):
-        # False = can continue
+        # True = Keep Running
         if(self._board.get_step_count() == pow(self._board.get_size(), 2)):
             self._gui.set_caption('Congratulation, You Solved The Problem!')
             print("Congratulation, You Solved The Problem!\nPath:")
@@ -37,8 +35,8 @@ class KnightsTour:
 
                     # check current tile see if any legel move avaliable.
                     self._board.check_legel_move(target_pos, target_pos)
-            except Board.NotSloveException:
-                print("Unfortunally, You Did Not Solved The Problem!\nPath:")
+            except Board.NoSloveException:
+                print("Unfortunally, Those Moves Cant Solved The Problem!\nPath:")
                 self._board.show_path()
                 return False
         
@@ -46,42 +44,38 @@ class KnightsTour:
     
     def _slove(self, algo):
         approach = Algorithm(self._board, algo)
-        return approach.get_result()
-
+        try:
+            return approach.get_result()
+        except Algorithm.NoSolutionException:
+            return list()
 
     def start(self):
         pygame.init()
-        pygame.display.set_caption('SIT-215 PBL2 KnightsTour [Group-6]')
-
-        # Set up the drawing window
-        self._gui = GUI(WINDOWS_SIZE, pygame, self._board.get_tiles())
         self._gui.set_caption('SIT-215 PBL2 KnightsTour [Group-6]')
 
-        # Run until the user asks to quit
         running = True
         self._gui.draw_board()
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                    break
 
                 if (DEBUG_MODE is True):
+                    # using cursor
                     running = self._curser_debug(event)
                 else:
-                    solution = self._slove(self._algo)
-                    if len(solution[0]) == pow(self._board.get_size(), 2):
-                        # play movement
-                        for move in solution[0]:
-                            target_pos = {'x': move[0], 'y': move[1]}
-                            self._board.move(target_pos)
-                            self._gui.draw_board()
-                            self._gui.draw_path(self._board.get_paths(), target_pos, self._board.get_size())
-                            self._gui.refresh()
-                            pygame.time.wait(1000)
-
-                        running = False
-
+                    # using algorithm
+                    solutions = self._slove(self._algo)
+                    if len(solutions) != 0:
+                        print("{} solution(s) found!".format(len(solutions)))
+                        # play moves
+                        self._gui.draw_paths(self._board, solutions)
+                        print(solutions)
+                    else:
+                        print("No Solution!")
+                    running = False
+                    break
                 self._gui.refresh()
-
         pygame.quit()
         
