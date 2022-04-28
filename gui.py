20c02b82e1909a245fe744a90b6fc44712c0ac6a
SIO TOU LAI [Nelson] <laisiotou1997@gmail.com>
2022-04-29 00:54:06 +1000

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,11 +1,15 @@
-
 import pygame
 from board import Board
+from tile import Tile
 
-LINE_COLOR = (255, 0, 0) # Red
-WAIT_TIME = 100
+LINE_COLOR = (255, 0, 0)  # Red
+WAIT_TIME = 100  # time to wait for each line to draw
+
+
 class GUI:
-    def __init__(self, size:list, pg:pygame, tiles) -> None:
+    """Class for GUI
+    """
+    def __init__(self, size: list, pg: pygame, tiles: Tile) -> None:
         self._tiles = tiles
         self._size = size
         self._rect = pg.Rect
@@ -13,12 +17,20 @@ class GUI:
         self._draw = pg.draw
         self._time = pg.time
         self._screen = self._display.set_mode(size)
+
         self.refresh()
-    
-    def fill(self, rgb:tuple):
+
+    def fill(self, rgb: tuple) -> None:
         self._screen.fill(rgb)
-    
-    def draw_path(self, pos_chain:list, target_pos:dict, board_size:int):
+
+    def draw_path(self, pos_chain: list, target_pos: dict, board_size: int):
+        """Draw single or mutiple line
+
+        Args:
+            pos_chain (list): posistions to draw
+            target_pos (dict): position to move
+            board_size (int): board size
+        """
         # get center point of the tile
         offset = int(self._size[0] / board_size)
 
@@ -26,14 +38,15 @@ class GUI:
         previous_pos.append(int(offset * pos_chain[0][1] + offset / 2))
         previous_pos.append(int(offset * pos_chain[0][0] + offset / 2))
 
+        # draw mutiple lines
         for pos in pos_chain:
             pos = [int(offset * pos[1] + offset / 2), int(offset * pos[0] + offset / 2)]
             self._draw.line(self._screen, LINE_COLOR, previous_pos, pos)
             previous_pos = pos
 
         target_center = []
-        target_center.append(int(offset * target_pos['x'] + offset / 2))
-        target_center.append(int(offset * target_pos['y'] + offset / 2))
+        target_center.append(int(offset * target_pos["x"] + offset / 2))
+        target_center.append(int(offset * target_pos["y"] + offset / 2))
 
         self._draw.line(self._screen, LINE_COLOR, previous_pos, target_center)
 
@@ -41,19 +54,19 @@ class GUI:
         for solution in solutions:
             first_move = True
             for move in solution:
-                if first_move is True :
+                if first_move is True:
                     # ignore first move
                     first_move = False
                     continue
 
-                target_pos = {'x': move[0], 'y': move[1]}
+                target_pos = {"x": move[0], "y": move[1]}
                 if board.move(target_pos) is True:
                     self.draw_board()
                     self.draw_path(board.get_paths(), target_pos, board.get_size())
                     self.refresh()
                     self._time.wait(WAIT_TIME)
                 else:
-                    raise Board.InvalidMoveException("Please Check Your Algorithm!") 
+                    raise Board.InvalidMoveException("Please Check Your Algorithm!")
 
     def set_caption(self, name):
         self._display.set_caption(name)
@@ -68,7 +81,13 @@ class GUI:
             for row in range(board_size):
                 color = self._tiles[column][row].get_color()
 
-                self._draw.rect(self._screen, color, self._rect(rect_width * column, rect_width * row, rect_width, rect_width))
+                self._draw.rect(
+                    self._screen,
+                    color,
+                    self._rect(
+                        rect_width * column, rect_width * row, rect_width, rect_width
+                    ),
+                )
 
     def refresh(self):
         self._display.flip()
