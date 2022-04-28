d3d042e3282b92fd0dba243bb101b55eb0e49e4d
SIO TOU LAI [Nelson] <laisiotou1997@gmail.com>
2022-04-29 00:55:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from knightstour import KnightsTour
-from algorithm import Algorithm
+from algorithm import KTAlgorithm
 
-BOARD_SIZE = (12, 12)
-START_POS = {'x': 0, 'y': 0}
+BOARD_SIZE = (6, 6)
+START_POS = {"x": 0, "y": 0}
+
 
 def main():
-    approach = Algorithm.Warnsdorff # replace with different approach algorithm
+    approach = KTAlgorithm.Warnsdorff  # replace with different algorithm
     KnightsTour(BOARD_SIZE, START_POS, approach)
+
 
 main()
